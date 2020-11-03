ff71779bf499e1ca0f5b2f67efb38b8ae3f379ac
Gavin <gavinbrrtt@gmail.com>
2020-11-03 09:40:38 -0800

    Added initial config files.

---
+++ src/compressor.py
@@ -0,0 +1,27 @@
+class Compressor:
+	
+	def __init__(self, string):
+		self.table = {}
+		self.encoded = []
+		self.words = string.split(' ')
+
+	def compress(self):
+		return self.count_words + self.encode_table()
+
+	def decompress(self):
+		...
+
+	def count_words(self):
+		idx = 0
+		for word in self.words:
+			if word in self.table:
+				self.encoded.append(self.table[word])
+				continue
+			else:
+				self.table.setdefault(word, idx)
+				self.encoded.append(idx)
+				idx += 1
+		return bytes(self.encoded)
+	
+	def encode_table(self):
+		return b'|' + '|'.join(self.table.keys()).encode()
